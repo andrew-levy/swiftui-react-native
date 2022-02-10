75e2bbffd80e7529f2e5cbfe66f4e576b280168d
Zach <zpg6@pitt.edu>
2022-02-09 21:50:55 -0500

    feat: added modifiers to ColorSubComponent

--- src/views/Color/Color.tsx
+++ src/views/Color/Color.tsx
@@ -3,7 +3,7 @@ import { Color as ColorType } from '../../utils/colors';
 import { ShapeModifiers } from '../../utils/modifiers';
 import { Rectangle } from '../Shapes';
 
-type ColorProps = Omit<ShapeModifiers,"fill"> & {
+type ColorProps = Omit<ShapeModifiers, 'fill'> & {
   red?: number;
   blue?: number;
   green?: number;
@@ -16,21 +16,27 @@ const colorSubComponents = {
 };
 
 // Example: <Color red={255} green={255} blue={255} />
-export const Color = ({ red = 0, green = 0, blue = 0, ...rest}: ColorProps) => {
-  return (
-    <Rectangle
-      fill={`rgb(${red},${green},${blue})`}
-      {...rest}
-    />
-  );
+export const Color = ({
+  red = 0,
+  green = 0,
+  blue = 0,
+  ...rest
+}: ColorProps) => {
+  return <Rectangle fill={`rgb(${red},${green},${blue})`} {...rest} />;
+};
+
+type ColorSubComponentProps = Omit<ShapeModifiers, 'fill'> & {
+  color: ColorType;
 };
 
 // Example: <Color.red />
-const ColorSubComponent = ({ color }: { color: ColorType }) => {
-  return <Rectangle frame={{ width: 100, height: 100 }} fill={color} />;
+const ColorSubComponent = ({ color, ...rest }: ColorSubComponentProps) => {
+  return <Rectangle fill={color} {...rest} />;
 };
 
 // Dynamically set the subcomponents
 Object.keys(colorSubComponents).forEach((color) => {
-  Color[color] = () => <ColorSubComponent color={colorSubComponents[color]} />;
+  Color[color] = ({ ...rest }: Omit<ShapeModifiers, 'fill'>) => (
+    <ColorSubComponent color={colorSubComponents[color]} {...rest} />
+  );
 });
