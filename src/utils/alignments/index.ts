c0b23550c8703517b0fd62a8cb223ef45c16300e
andrew-levy <alevy97@gmail.com>
2022-02-14 18:30:39 -0500

    fix for bottom alignment

--- src/utils/alignments/index.ts
+++ src/utils/alignments/index.ts
@@ -27,7 +27,7 @@ export const AlignmentMap = {
     leading: { justifyContent: 'center', alignItems: 'flex-start' },
     trailing: { justifyContent: 'center', alignItems: 'flex-end' },
     top: { justifyContent: 'flex-start', alignItems: 'center' },
-    bottom: { justifyContent: 'flex-start', alignItems: 'center' },
+    bottom: { justifyContent: 'flex-end', alignItems: 'center' },
     topLeading: { justifyContent: 'flex-start', alignItems: 'flex-start' },
     topTrailing: { justifyContent: 'flex-start', alignItems: 'flex-end' },
     bottomLeading: { justifyContent: 'flex-end', alignItems: 'flex-start' },
@@ -39,5 +39,5 @@ export const getAlignment = (
   alignment: ZStackAlignment | VStackAlignment | HStackAlignment,
   stackType: 'hstack' | 'vstack' | 'zstack'
 ) => {
-  return AlignmentMap[stackType][alignment || 'center'];
+  return AlignmentMap[stackType][alignment];
 };
