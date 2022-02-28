f3ca231dc8751612b9fc0bff2b0f9c7a6cf7c5cd
andrew-levy <alevy97@gmail.com>
2022-02-28 17:29:09 -0500

    Added env keys type to index.ts

--- src/hooks/useEnvironment.ts
+++ src/hooks/useEnvironment.ts
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
 import { SwiftUIRootContext } from '../views/SwiftUIRoot/SwiftUIRoot';
 
-export type EnvironmentKey<T> = 'colorScheme' | keyof T;
+export type EnvironmentKeys<T> = 'colorScheme' | keyof T;
 
 export const useEnvironment = <T>(key: T) => {
   const { envs, setEnv } = useContext(SwiftUIRootContext);
-  return [envs[key], (value: string) => setEnv(key, value)];
+  return [envs[key], (value: any) => setEnv(key, value)];
 };
