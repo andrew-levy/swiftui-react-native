0aa89ea1fc7525449d291fb8effa3d4f89417b1a
andrew-levy <alevy97@gmail.com>
2022-02-28 16:56:45 -0500

    added strict types to useEnv

--- src/hooks/useEnvironment.ts
+++ src/hooks/useEnvironment.ts
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { SwiftUIRootContext } from '../views/SwiftUIRoot/SwiftUIRoot';
 
+export type EnvironmentKey<T> = 'colorScheme' | keyof T;
+
 export const useEnvironment = <T>(key: T) => {
   const { envs, setEnv } = useContext(SwiftUIRootContext);
   return [envs[key], (value: string) => setEnv(key, value)];
