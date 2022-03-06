79267de2640e05eb415a6355a0bc0e9a67658c81
andrew-levy <alevy97@gmail.com>
2022-03-06 17:10:49 -0500

    added env hook error logic

--- src/hooks/useColorScheme.ts
+++ src/hooks/useColorScheme.ts
@@ -2,5 +2,5 @@ import { useContext } from 'react';
 import { SwiftUIRootContext } from '../views/SwiftUIRoot/SwiftUIRoot';
 
 export const useColorScheme = () => {
-  return useContext(SwiftUIRootContext).envs.colorScheme || 'light';
+  return useContext(SwiftUIRootContext)?.envs?.colorScheme || 'light';
 };
--- src/hooks/useEnvironment.ts
+++ src/hooks/useEnvironment.ts
@@ -4,10 +4,17 @@ import { SwiftUIRootContext } from '../views/SwiftUIRoot/SwiftUIRoot';
 type EnvironmentKeys<T> = 'colorScheme' | keyof T;
 
 export const useEnvironment = <T, V>(key: EnvironmentKeys<T>) => {
-  const { envs, setValueAtKey } = useContext(SwiftUIRootContext) as unknown as {
+  const ctx = useContext(SwiftUIRootContext) as unknown as {
     envs: T;
     setValueAtKey: (key: string, value: V) => void;
   };
+  if (!ctx) {
+    console.warn(
+      'In order to use useEnvironment, you must wrap your component in a SwiftUIRoot.'
+    );
+    return [undefined, () => {}];
+  }
+  const { envs, setValueAtKey } = ctx;
   return [
     envs[key as keyof T] as unknown as V,
     (value: V) => setValueAtKey(key as string, value as unknown as V),
