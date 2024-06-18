6c89acc496676ef809dd1dd4e1516d1b5fb916bd
betomoedano <betomoedano@outlook.com>
2024-06-18 14:27:56 -0500

    remove unnecessary plugin

--- example/babel.config.js
+++ example/babel.config.js
@@ -3,31 +3,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'expo-router/babel',
-      [
-        'module-resolver',
-        {
-          extensions: ['.tsx', '.ts', '.js', '.json'],
-
-          alias: {
-            'swiftui-react-native/experimental': path.join(
-              __dirname,
-              '..',
-              'src',
-              'experimental',
-              'index.ts'
-            ),
-            // For development, we want to alias the library to the source
-            'swiftui-react-native': path.join(
-              __dirname,
-              '..',
-              'src',
-              'index.ts'
-            ),
-          },
-        },
-      ],
-    ],
+    plugins: [],
   };
 };
